858fbd0224d57b6b5f01af2095b36eb418a07b64
debanjanBQ <debanjan@bitqit.com>
2021-03-02 13:13:41 +0530

    added link routes controllers and object ref of links in users

--- src/ioc/DIContainer.ts
+++ src/ioc/DIContainer.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { Container } from 'inversify';
 import UserController from '../controllers/Usercontroller';
 import Crypt from '../crypto/crypt';
+import LinksRepository from '../repository/LinksRepository';
 import UserRepository from '../repository/UserRepository';
 import Middleware from '../routes/middleware';
 import Routes from '../routes/routes';
+import LinkController from '../controllers/LinkController';
 
 class DIContainer {
 
@@ -32,6 +34,8 @@ class DIContainer {
             container.bind<Crypt>(Crypt).toSelf();
             container.bind<UserController>(UserController).toSelf();
             container.bind<Middleware>(Middleware).toSelf();
+            container.bind<LinksRepository>(LinksRepository).toSelf();
+            container.bind<LinkController>(LinkController).toSelf();
 
             const router = Router();
             container.bind<Router>(Router).toConstantValue(router);
